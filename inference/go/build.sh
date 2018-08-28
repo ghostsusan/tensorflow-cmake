5f0f23d6df0599d300c93ef6fcec512c54e795ed
PatWie <mail@patwie.com>
2018-08-28 22:49:46 +0200

    simplify go build

--- inference/go/build.sh
+++ inference/go/build.sh
@@ -1,9 +1,23 @@
-TF_PATH=`python -c "import tensorflow as tf; print(tf.__path__[0])"`
+TF_TYPE="cpu" # Change to "gpu" for GPU support
+
+
+if [ -d "external" ]; then
+  echo "skip download"
+else
+  mkdir -p external
+  cd external
+  curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-$(go env GOOS)-x86_64-1.10.1.tar.gz" | tar -C . -xz
+  cd ..
+fi
+
+TF_PATH=`realpath external/lib`
+
+
 export LD_LIBRARY_PATH=${TF_PATH}:${LD_LIBRARY_PATH}
 export LIBRARY_PATH=${TF_PATH}:${LIBRARY_PATH}
 
 go build inference_go.go
 
 echo "you need to set the following paths:"
-echo "export LD_LIBRARY_PATH=${TENSORFLOW_BUILD_DIR}/:\${LD_LIBRARY_PATH}"
 echo "export LD_LIBRARY_PATH=${TF_PATH}/:\${LD_LIBRARY_PATH}"
+
